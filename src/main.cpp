eb24e1f11ec7387a717565ad4851fbf7bd15eac6
Klaudia Barańska <klaudiabaranska11@gmail.com>
2021-03-12 16:53:44 +0100

    3.4 Testowanie działania aplikacji

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,24 @@ int main() {
 
 
 	#ifdef USE_TRIGONOMETRY_DEGREE
-	double sin_calc = degreemath::sin(45);
+	double sin_calc = degreemath::sin(45.0);
 	std::cout << sin_calc << std::endl;
-	double cos_calc = degreemath::cos(30);
+	double cos_calc = degreemath::cos(30.0);
 	std::cout << cos_calc << std::endl;
-	double tg_calc = degreemath::tg(60);
+	double tg_calc = degreemath::tg(60.0);
 	std::cout << tg_calc << std::endl;
-	double ctg_calc = degreemath::ctg(30);
+	double ctg_calc = degreemath::ctg(30.0);
 	std::cout << ctg_calc << std::endl;
 	double result = degreemath::sin(45.0);
 	#else
-	double cos_calc =cos(M_PI/4.0);
+	double sin_calc =sin(M_PI / 4.0);
+	std::cout << sin_calc << std::endl;
+	double cos_calc =cos(M_PI/6.0);
 	std::cout << cos_calc << std::endl;
+	double tg_calc = tan(M_PI/3.0);
+	std::cout << tg_calc << std::endl;
+	double ctg_calc = 1/tan(M_PI / 4.0);
+	std::cout << ctg_calc << std::endl;
 	#endif
 
 }
