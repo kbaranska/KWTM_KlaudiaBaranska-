91de8cb13f6e5bacc5deaede887ae0f53bf03bd5
Klaudia Barańska <klaudiabaranska11@gmail.com>
2021-03-12 16:15:55 +0100

    3.3a Sterowanie procesem kompilacji - włączona opcja

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
-#include "trygonometria.h"
 #include <iostream>
+#include <LaborkaConfig.h>
+
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include "trygonometria.h"
+#else
+#include <cmath>
+#define _USE_MATH_DEFINES
+#endif
+
 int main() {
-	double sin_calc =degreemath::sin(45);
+
+
+	#ifdef USE_TRIGONOMETRY_DEGREE
+	double sin_calc = degreemath::sin(45);
 	std::cout << sin_calc << std::endl;
-	double cos_calc= degreemath::cos(30);
-	std::cout <<cos_calc << std::endl;
-	double tg_calc =degreemath::tg(60);
+	double cos_calc = degreemath::cos(30);
+	std::cout << cos_calc << std::endl;
+	double tg_calc = degreemath::tg(60);
 	std::cout << tg_calc << std::endl;
-	double ctg_calc =degreemath::ctg(30);
+	double ctg_calc = degreemath::ctg(30);
 	std::cout << ctg_calc << std::endl;
+	double result = degreemath::sin(45.0);
+	#else
+	double cos_calc =cos(M_PI/4.0);
+	std::cout << cos_calc << std::endl;
+	#endif
 
 }
